6e25b75697621810a9df2f38b218bca5d3f06f1b
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-14 20:13:40 +0600

    Creating Category and Post

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . import forms
 # Create your views here.
 
 def add_category(request):
-    pass
+    if request.method=='POST':
+        category_form=forms.CategoryForm(request.POST)
+        if category_form.is_valid():
+          category_form.save()
+          redirect('add_category')
+    else:
+        category_form=forms.CategoryForm()
+    return render(request,'add_category.html',{'form':category_form})
